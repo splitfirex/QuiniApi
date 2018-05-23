d3613c7f8beb2e30c5bcdd818a0cafaafe8622b2
Daniel Fariña <splitfirex@gmail.com>
2018-05-23 19:26:10 +0200

    cambio bddd

--- dbconn.js
+++ dbconn.js
@@ -1,2 +1,3 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', { autoIndex: false });
+mongoose.connect('mongodb://9632dd8367424d270e559f1d508d6dbb:9632dd8367424d270e559f1d508d6dbb@6a.mongo.evennode.com:27017,6b.mongo.evennode.com:27017/9632dd8367424d270e559f1d508d6dbb?replicaSet=eu-6', { autoIndex: false })
+.then((err) => console.log(err));
